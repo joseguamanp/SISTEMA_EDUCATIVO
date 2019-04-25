02c22c81d6b39dc9ab2cb0ee36ba3ab72b8d0537
Nestor Cruz <nes7or.cruz@hotmail.com>
2019-04-25 18:10:12 -0500

    mantenimiento de sexo arreglado

--- app/Http/Controllers/GeneralController/SexoController.php
+++ app/Http/Controllers/GeneralController/SexoController.php
@@ -10,112 +10,132 @@ use App\Http\Requests\SexoRequest;
 
 class SexoController extends Controller
 {
-    public function __construct()
-    {
-        $this->middleware('auth');
-        $this->middleware('admin');
-    }
-    public function index()
-    { 
-        $datos = SexoModel::withTrashed()->get();
-        return view('admin.listasenescyt.datosidentificacion.createsexo',['datos'=>$datos]);
-    }
+  /**
+  * [__construct description]
+  */
+  public function __construct()
+  {
+    $this->middleware('auth');
+    $this->middleware('admin');
+  }
 
-    /**
-     * Show the form for creating a new resource.
-     *
-     * @return \Illuminate\Http\Response
-     */
-    public function create()
-    {
-        //
-    }
+  /**
+  * Display a listing of the resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function index()
+  {
+    $datos = SexoModel::withTrashed()->get();
+    return view('admin.listasenescyt.datosidentificacion.createsexo',['datos'=>$datos]);
+  }
 
-    /**
-     * Store a newly created resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @return \Illuminate\Http\Response
-     */
-    public function store(SexoRequest $request)
-    {
-        $next=SexoModel::withTrashed()->max('id');
-        if($next == 0)
-            $next = 1;
-        else
-            $next = $next + 1;
-        $valor=strtoupper($request->input('etiqueta'));
-        $this->validar($valor,$next);
-        return $this->index();
-    }
-    public function validar($valor,$contar)
-    {
-        //$id_user=Auth::user()->id;
-        $base=SexoModel::create([
-                'id'=>$contar,
-                'id_usu_cre' => Auth::user()->id,
-                'etiqueta'=>$valor,
-            ]);
-    }
-    /**
-     * Display the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function show($id)
-    {
-        //
-    }
+  /**
+  * Show the form for creating a new resource.
+  *
+  * @return \Illuminate\Http\Response
+  */
+  public function create()
+  {
+    //
+  }
 
-    /**
-     * Show the form for editing the specified resource.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function edit($id)
-    {
-        $datosid = SexoModel::find($id);
-        return view('admin.listasenescyt.datosidentificacion.editsexo',['editaret'=>$datosid]);
-    }
+  /**
+  * [store description]
+  * @param  SexoRequest $request [description]
+  * @return [type]               [description]
+  */
+  public function store(SexoRequest $request)
+  {
+    $next=SexoModel::withTrashed()->max('id');
+    if($next == 0) $next = 1;
+    else $next = $next + 1;
+    $valor=strtoupper($request->input('etiqueta'));
+    $this->validar($valor,$next);
+    return $this->index();
+  }
 
-    /**
-     * Update the specified resource in storage.
-     *
-     * @param  \Illuminate\Http\Request  $request
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function update(SexoRequest $request, $id)
-    {
-        $datoset = SexoModel::find($id);
-        $datoset->etiqueta = strtoupper($request->input('etiqueta'));
-        $datoset->id_usu_mod = Auth::user()->id;
-        $datoset->save();
-        return redirect('/admin/sexo/');
-    }
+  /**
+  * [validar description]
+  * @param  [type] $valor  [description]
+  * @param  [type] $contar [description]
+  * @return [type]         [description]
+  */
+  public function validar($valor,$contar)
+  {
+    //$id_user=Auth::user()->id;
+    $base=SexoModel::create([
+      'id'=>$contar,
+      'id_usu_cre' => Auth::user()->id,
+      'etiqueta'=>$valor,
+    ]);
+  }
 
-    /**
-     * Remove the specified resource from storage.
-     *
-     * @param  int  $id
-     * @return \Illuminate\Http\Response
-     */
-    public function destroy($id)
-    {
-        $datoset=SexoModel::find($id);
-        $datoset->id_usu_mod = Auth::user()->id;
-        $datoset->save();
-        $datoset->delete();
-        return redirect('/admin/sexo/');
-    }
-    public function restaurar($id)
-    {
-        $datos=SexoModel::onlyTrashed()->find($id)->restore();
-        $datoset=SexoModel::find($id);
-        $datoset->id_usu_mod = Auth::user()->id;
-        $datoset->save();
-        return redirect('/admin/sexo/');
-    }
+  /**
+  * Display the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function show($id)
+  {
+    //
+  }
+
+  /**
+  * Show the form for editing the specified resource.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function edit($id)
+  {
+    $datosid = SexoModel::find($id);
+    return view('admin.listasenescyt.datosidentificacion.editsexo',['editaret'=>$datosid]);
+  }
+
+  /**
+  * Update the specified resource in storage.
+  *
+  * @param  \Illuminate\Http\Request  $request
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function update(SexoRequest $request, $id)
+  {
+    $datoset = SexoModel::find($id);
+    $datoset->etiqueta = strtoupper($request->input('etiqueta'));
+    $datoset->id_usu_mod = Auth::user()->id;
+    $datoset->save();
+    return redirect('/admin/sexo/');
+  }
+
+  /**
+  * Remove the specified resource from storage.
+  *
+  * @param  int  $id
+  * @return \Illuminate\Http\Response
+  */
+  public function destroy($id)
+  {
+    $datoset=SexoModel::find($id);
+    $datoset->id_usu_mod = Auth::user()->id;
+    $datoset->save();
+    $datoset->delete();
+    return redirect('/admin/sexo/');
+  }
+
+  /**
+  * [restaurar description]
+  * @param  [type] $id [description]
+  * @return [type]     [description]
+  */
+  public function restaurar($id)
+  {
+    $datos=SexoModel::onlyTrashed()->find($id)->restore();
+    $datoset=SexoModel::find($id);
+    $datoset->id_usu_mod = Auth::user()->id;
+    $datoset->save();
+    return redirect('/admin/sexo/');
+  }
 }
