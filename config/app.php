a5ce06bc505de26df11cce2c7fa6a8a7f42de3c4
joseguamanp <arturoguaman96@gmail.com>
2019-05-01 18:26:11 -0500

    Update app.php

--- config/app.php
+++ config/app.php
@@ -160,7 +160,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
-        UxWeb\SweetAlert\SweetAlertServiceProvider::class,
+        //UxWeb\SweetAlert\SweetAlertServiceProvider::class,
     ],
 
     /*
@@ -211,7 +211,7 @@ return [
         'View' => Illuminate\Support\Facades\View::class,
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
-        'Alert' => UxWeb\SweetAlert\SweetAlert::class,
+        //'Alert' => UxWeb\SweetAlert\SweetAlert::class,
     ],
 
 ];
